922030272fcdf987fb685c822c0b1f0b1b301a4a
Beres Ferenc <ferdzso05@gmail.com>
2022-10-25 07:49:02 +0000

    first component tests added

--- python/components.py
+++ python/components.py
@@ -3,39 +3,52 @@ import uuid
 import numpy as np
 
 class Network:
-    def __init__(self, num_nodes: int=100, k: int=5):
+    def __init__(self, num_nodes: int=100, k: int=5, graph: nx.Graph=None):
         """Peer-to-peer network abstraction"""
         self.num_nodes = num_nodes
         self.k = k
-        self.generate_graph()
+        self.generate_graph(graph)
         
-    def generate_graph(self):
+    def generate_graph(self, graph: nx.Graph=None):
         success = False
-        self.graph = nx.random_regular_graph(self.k, self.num_nodes)
+        if graph is not None:
+            self.graph = graph
+            self.num_nodes = self.graph.number_of_nodes()
+            self.k = -1
+        else:
+            self.graph = nx.random_regular_graph(self.k, self.num_nodes)
         # NOTE: implement custom edge weights
         self.weights = dict(zip(self.graph.edges, np.random.random(self.graph.number_of_edges())))
+
+class Record:
+    def __init__(self, node: int, delay: float, hops: int):
+        """Message information propagated through the peer-to-peer network"""
+        self.node = node
+        self.delay = delay
+        self.hops = hops
         
 class Message:
     def __init__(self, sender: int):
+        """Abstraction for Ethereum transactions"""
         self.mid = uuid.uuid4().hex
         self.sender = sender
-        # NOTE: implement custom latency calculation
-        self.start = 0.0
-        self.history = dict()
-        # initial state
-        self.queue = [(self.sender, self.start)]
+        self.history = {sender:Record(self.sender, 0.0, 0)}
+        self.queue = [sender]
         
     def process(self, network: Network):
+        """Propagate the message on outbound links"""
         new_queue = []
-        for node, elapsed_time in self.queue:
+        for node in self.queue:
+            record = self.history[node]
             # NOTE: implement custom neighbor calculation
             for neigh in network.graph.neighbors(node):
                 if not neigh in self.history:
                     link = (node, neigh)
                     if not link in network.weights:
                         link = (neigh, node)
-                    current_time = elapsed_time + network.weights[link]
-                    self.history[neigh] = current_time
-                    new_queue.append((neigh, current_time))
+                    elapsed_time = record.delay + network.weights[link]
+                    new_record = Record(neigh, elapsed_time, record.hops+1)
+                    self.history[neigh] = new_record
+                    new_queue.append(neigh)
         self.queue = new_queue
         return len(self.history) / network.num_nodes
---
+++ tests/test_components.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.insert(0, '../python')
+import networkx as nx
+from components import Network, Message
+
+def test_custom_graph():
+    G = nx.Graph()
+    G.add_edges_from([(0,1),(0,2),(0,3),(1,4),(4,5)])
+    net = Network(graph=G)
+    assert net.num_nodes == 6
+    assert net.k == -1
+    assert len(net.weights) == 5
+    
+def test_single_message():
+    G = nx.Graph()
+    G.add_edges_from([(0,1),(0,2),(0,3),(1,4),(4,5)])
+    net = Network(graph=G)
+    # start a message from the middle
+    msg = Message(0)
+    first_ratio = msg.process(net)
+    assert (first_ratio - 2/3) < 0.0001
+    second_ratio = msg.process(net)
+    assert (second_ratio - 5/6) < 0.0001
+    third_ratio = msg.process(net)
+    assert third_ratio == 1.0
+    assert msg.history[0].hops == 0
+    assert msg.history[1].hops == 1
+    assert msg.history[2].hops == 1
+    assert msg.history[3].hops == 1
+    assert msg.history[4].hops == 2
+    assert msg.history[5].hops == 3
